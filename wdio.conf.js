1fbc460c0780316db48ea047b1e0dd0a05f0fc73
pdelboca <patriciodelboca@gmail.com>
2024-07-25 10:09:05 +0200

    Switch to electron service

--- wdio.conf.js
+++ wdio.conf.js
@@ -50,7 +50,7 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome',
+        browserName: 'electron',
         'goog:chromeOptions': {
           args: ['headless', 'no-sandbox'],
         },
@@ -102,7 +102,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: [],
+    services: ['electron'],
 
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
