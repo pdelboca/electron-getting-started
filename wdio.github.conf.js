14b62010531f9d64cb7a267b1f8f749f1c85bb2a
pdelboca <patriciodelboca@gmail.com>
2024-07-25 09:38:15 +0200

    One last try

--- wdio.github.conf.js
+++ wdio.github.conf.js
@@ -53,7 +53,7 @@ exports.config = {
         browserName: 'chrome',
         browserVersion: '126.0.6478.182',
         'goog:chromeOptions': {
-          args: ['headless', 'disable-gpu'],
+          args: ['headless', 'no-sandbox', 'disable-gpu'],
         },
     }],
     //
